cd00df63b946ebb7dfe9dc7f0c46aef4f2d4692a
julio <julionavarro-09@hotmail.com>
2023-02-23 04:04:37 -0300

    Finalizado a criação das classes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -15,16 +17,35 @@ public class Main {
         curso2.setDescricao("Descrição do curso Javinha");
         curso2.setCargaHoraria(10);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+       // System.out.println(curso1);
+        //System.out.println(curso2);
 
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Mentoria de Java");
         mentoria1.setDescricao("Descrição da mentoria de Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(mentoria1);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("descricao do bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
 
+        Dev devJulio = new Dev();
+        devJulio.setNome("Julio");
+        devJulio.inscreverBootcamp(bootcamp);
+        System.out.println("conteudos Incritos Julio: " + devJulio.getConteudosInscritos());
+        devJulio.progredir();
+        devJulio.progredir();
+        System.out.println("conteudos Concluidos Julio: " + devJulio.getConteudosConcluidos());
+
+        Dev devGabs = new Dev();
+        devGabs.setNome("gabs");
+        devGabs.inscreverBootcamp(bootcamp);
+        System.out.println("conteudos Incritos Gabs: " + devGabs.getConteudosInscritos());
+        devGabs.progredir();
+        System.out.println("conteudos Concluidos Gabs: " + devGabs.getConteudosConcluidos());
 
     }
 
